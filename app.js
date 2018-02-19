3717fbd832b9df6a17e91520d8e11c97a572ebcc
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-19 13:25:40 +0530

    1. Changes app.js.

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-// var Built = require('built-extension-sdk')
-
-// // Initiate application
-// var app = Built.App('blta3145e1287bd1c7b')
-// // .setHost('localhost')
-// // .setPort(8000)
-// .setHost("test-stag-api.built.io")
-// .setProtocol("https")
-// // .setPort(80)
-// .setMasterKey('bltb14b939d2d820353')
-
-// var extensionSDK = app.Extension({
-// 	secret_key     : 'app_test_123',
-// 	extension_key	 : 'blt_ext_default',
-// 	static         : __dirname + '/client',
-// 	routes         : require('./server/routes')
-// })
-
-// return extensionSDK.start(9000
-
-// Wrong code
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt8f9dff6a8e3d329c')
+var app = Built.App('blta3145e1287bd1c7b')
 // .setHost('localhost')
 // .setPort(8000)
-.setHost("dev-api.built.io")
+.setHost("test-stag-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('blt89bb8dd35204a8dc')
+.setMasterKey('bltb14b939d2d820353')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blta8c9b5ed7cdcfc35',
+	secret_key     : 'app_test_123',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
 })
 
 return extensionSDK.start(9000)
+
+// Wrong code
+// var Built = require('built-extension-sdk')
+
+// // Initiate application
+// var app = Built.App('blt8f9dff6a8e3d329c')
+// // .setHost('localhost')
+// // .setPort(8000)
+// .setHost("dev-api.built.io")
+// .setProtocol("https")
+// // .setPort(80)
+// .setMasterKey('blt89bb8dd35204a8dc')
+
+// var extensionSDK = app.Extension({
+// 	secret_key     : 'blta8c9b5ed7cdcfc35',
+// 	extension_key	 : 'blt_ext_default',
+// 	static         : __dirname + '/client',
+// 	routes         : require('./server/routes')
+// })
+
+// return extensionSDK.start(9000)
 
 
 // e2c7844b237643927ec4e8b21bc8204780c47e60
