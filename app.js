58fafd7e3703df6945c8dc89949ac3e2c41e7afa
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-19 13:03:18 +0530

    1. Changes app.js and index.html.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blta3145e1287bd1c7b')
+var app = Built.App('blt8f9dff6a8e3d329c')
 // .setHost('localhost')
 // .setPort(8000)
-.setHost("test-stag-api.built.io")
+.setHost("dev-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('bltb14b939d2d820353')
+.setMasterKey('blt89bb8dd35204a8dc')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'app_test_123',
+	secret_key     : 'blta8c9b5ed7cdcfc35',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
