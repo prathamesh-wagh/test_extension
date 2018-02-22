74ec9e93fc21decf47a364659b34c3b122913e51
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-22 18:05:17 +0530

    sds

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// var Built = require('built-extension-sdk')
-
-// // Initiate application
-// var app = Built.App('blta3145e1287bd1c7b')
-// // .setHost('localhost')
-// // .setPort(8000)
-// .setHost("test-stag-api.built.io")
-// .setProtocol("https")
-// // .setPort(80)
-// .setMasterKey('bltb14b939d2d820353')
-
-// var extensionSDK = app.Extension({
-// 	secret_key     : 'app_test_123',
-// 	extension_key	 : 'blt_ext_default',
-// 	static         : __dirname + '/client',
-// 	routes         : require('./server/routes')
-// })
-
-// return extensionSDK.start(9000)
-
 // Wrong code
 var Built = require('built-extension-sdk')
 
